ddb2fbcd9508bc429e50ad4ba98e8b4a33e6cd4f
Jahnavi <jahnaviraman186@gmail.com>
2023-03-07 12:15:11 +0530

    completed all methods of doubly linked list

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -12,6 +12,7 @@ class DoublyLinkedist:
         self.length = 1
 
     def print_list(self):
+        '''prints all the node values in the linked list'''
         curr = self.head
         while curr is not None:
             print(curr.value)
@@ -30,6 +31,19 @@ class DoublyLinkedist:
         self.length += 1
         return True
     
+    def prepend(self, value):
+        '''adds a node at the beginning of the linked list'''
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+    
     def pop(self):
         '''remove a node from the end of the list'''
         if self.length == 0:
@@ -44,15 +58,83 @@ class DoublyLinkedist:
             temp.prev = None
         self.length -=1
         return temp
+    
+    def pop_first(self):
+        '''removes the first node from the linked list'''
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            temp.next = None
+            self.head.prev = None
+        self.length -= 1
+        return temp
+    
+    def get(self,index):
+        '''returns the node at the given index'''
+        # this method can be implemented similar to the one for singly linked list. But an optimised
+        # approach is implemented here.
+        if index < 0 or index >= self.length:
+            return None
+        if index < self.length/2:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length-1, index, -1):
+                temp = temp.prev
+        return temp
+    
+    def set_value(self, index, value):
+        '''set the value at the given index'''
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index-1)
+        after = before.next
+        new_node.next = after
+        new_node.prev = before
+        before.next = new_node
+        after.prev = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        '''removes the node at the given index'''
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+        temp = self.get(index)
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        # before = temp.prev
+        # after = temp.next
+        # before.next = after
+        # after.prev = before
+        temp.next, temp.prev = None, None
+        self.length -= 1
+        return temp
 
 doubly_linked_list = DoublyLinkedist(1)
 doubly_linked_list.append(2)
-doubly_linked_list.append(3)
-doubly_linked_list.append(4)
+doubly_linked_list.remove(1)
 doubly_linked_list.print_list()
-print('\n')
-print(doubly_linked_list.pop())
-print(doubly_linked_list.pop())
-print(doubly_linked_list.pop())
-print(doubly_linked_list.pop())
-print(doubly_linked_list.pop())
