df3721945e1634972789d3410d8d399433c36198
Jahnavi <jahnaviraman186@gmail.com>
2024-05-28 16:56:56 +0530

    added bubble sort and selection sort

--- Sorting.py
+++ Sorting.py
@@ -3,6 +3,65 @@ class Sorting:
         self.sorting_array = input_array
         self.comparison_count = 0
 
+    def bubble_sort(self):
+        '''
+        pushes the max element in each iteration of the outer loop by comparing the adjacent elements in inner loop
+        TC : O(N^2) - worst, average
+             O(N) - best
+        '''
+        n = len(self.sorting_array)
+        for i in range(n-1,0,-1):
+            isSwapped = 0
+            for j in range(i):
+                if self.sorting_array[j] > self.sorting_array[j+1]:
+                    temp = self.sorting_array[j]
+                    self.sorting_array[j+1] = self.sorting_array[j]
+                    self.sorting_array[j] = temp
+                    isSwapped = 1
+            if isSwapped == 0:
+                break
+                
+
+
+    def selection_sort(self):
+        '''
+        The array is divided into two parts: sorted half and unsorted half. In each iteration, the smallest element form the unsorted half is 
+        selected and swapped with the firstvelement of the unsorted part of the array. In this way, the size of the sorted half increases and
+        the arrays is sorted.
+        TC: O(n^2) - Best, Worst, Average
+        SC: O(1)
+        '''
+        n = len(self.sorting_array)
+        for i in range(n-1):
+            min_index = i
+            for j in range(i, n):
+                if self.sorting_array[j] < self.sorting_array[min_index]:
+                    min_index = j
+            self.sorting_array[i], self.sorting_array[min_index] = self.sorting_array[min_index], self.sorting_array[i]
+
+    def insertion_sort(self):
+        '''
+        place the element in its correct position in the sorted left half in each iteration. Check the elements towards the left with the current
+        element and swap until the current is placed in its correct postion.
+        TC : O(N^2) - worst, average
+             O(N) - best
+        '''
+        arr = self.sorting_array
+        count = self.comparison_count
+        n = len(arr)
+        for i in range(1,n):
+            x = arr[i]
+            j = i-1
+            while j >= 0 and arr[j]>x:
+                count += 1
+                arr[j+1] = arr[j]
+                j = j-1
+            arr[j+1] = x
+            if j >= 0:
+                count += 1
+        self.comparison_count = count
+
+
     def merge(self, p, q, r):
         arr = self.sorting_array
         nL = q-p+1
@@ -86,22 +145,12 @@ class Sorting:
             # n -= 1
             self.max_heapify(i, 0)
 
-    def insertion_sort(self):
-        arr = self.sorting_array
-        count = self.comparison_count
-        n = len(arr)
-        for i in range(1,n):
-            x = arr[i]
-            j = i-1
-            while j >= 0 and arr[j]>x:
-                count += 1
-                arr[j+1] = arr[j]
-                j = j-1
-            arr[j+1] = x
-            if j >= 0:
-                count += 1
-        self.comparison_count = count
+    
 
+arr = [1,]
+sorting_algo = Sorting(arr)
+sorting_algo.bubble_sort()
+print(sorting_algo.sorting_array)
 
 
         
