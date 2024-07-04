24f5b5d1b4548c4f99ec1f2ecd6dec93bd3af6ac
Jahnavi <jahnaviraman186@gmail.com>
2024-07-04 19:25:50 +0530

    added DFS recursion code

--- graphs.py
+++ graphs.py
@@ -76,8 +76,62 @@ class Graph:
 n = 9
 m = 9 
 edges = [[1,2],[1,6],[2,3],[2,4],[6,7],[6,9],[4,5],[7,8] ,[5,8]]
-graph = Graph(n,m,edges)
+#graph = Graph(n,m,edges)
 #graph.displayGraph()
-print(graph.DFSGraph(1))
+#print(graph.DFSGraph(1))
 
 # print([[0 for _ in range(5)] for _ in range(5)])
+
+def BFS(root, adjList):
+    res = []
+    queue = [root]
+    visited = [0 for _ in range(len(adjList))]
+
+    visited[root] = 1
+
+    while queue:
+        top = queue.pop(0)
+        res.append(top)
+        for i in adjList[top]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
+
+    return res
+
+
+def DFS(root, adjList):
+    res = []
+    stack = [root]
+    visited = [0 for _ in range(len(adjList))]
+
+    while stack:
+        node = stack.pop()
+        visited[node] = 1
+        res.append(node)
+
+        for i in adjList[node]:
+            if visited[i] == 0:
+                stack.append(i) 
+
+def DFS_recursion(root, adjList):
+    visited = [0 for _ in range(len(adjList))]
+    res = []
+    dfs(root, visited, res, adjList)
+
+def dfs(root, visited, res, adjList):
+    visited[root] = 1
+    res.append(root)
+
+    for i in adjList[root]:
+        if visited[i] == 0:
+            dfs(i)
+            
+
+
+
+adjList = [[1,2],[0, 4,3], [0,3],[1,2],[1]]
+
+print(BFS(0, adjList))
+
+
